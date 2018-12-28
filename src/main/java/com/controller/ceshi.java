d9dfb8d59fa40104d8a61d8ac996afcf33fedc2b
luo <1424918337@qq.com>
2018-12-28 14:22:46 +0800

    frst one

---
+++ src/main/java/com/controller/ceshi.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ceshi {
+
+    @RequestMapping("/index")
+    public String kok(){
+        return "index";
+    }
+}
